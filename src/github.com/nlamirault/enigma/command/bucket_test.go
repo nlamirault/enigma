fc5bfa172663bb97c7141f33471b5292864c89d9
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2015-10-19 13:25:35 +0200

    unit tests
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ src/github.com/nlamirault/enigma/command/bucket_test.go
@@ -0,0 +1,87 @@
+// Copyright (C) 2015 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package command
+
+import (
+	"testing"
+
+	"github.com/mitchellh/cli"
+)
+
+func TestBucketWithoutAction(t *testing.T) {
+	ui := new(cli.MockUi)
+	c := &BucketCommand{
+		UI: ui,
+	}
+
+	args := []string{
+		"--bucket", "foo",
+		"--region", "region1",
+	}
+
+	if code := c.Run(args); code != 1 {
+		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	}
+}
+
+func TestBucketWithoutBucket(t *testing.T) {
+	ui := new(cli.MockUi)
+	c := &BucketCommand{
+		UI: ui,
+	}
+
+	args := []string{
+		"--bucket", "foo",
+		"--region", "region1",
+		"--action", "get-text",
+	}
+
+	if code := c.Run(args); code != 1 {
+		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	}
+}
+
+func TestBucketWithoutRegion(t *testing.T) {
+	ui := new(cli.MockUi)
+	c := &BucketCommand{
+		UI: ui,
+	}
+
+	args := []string{
+		"--region", "region1",
+		"--action", "get-text",
+	}
+
+	if code := c.Run(args); code != 1 {
+		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	}
+}
+
+func TestBucketWithInvalidAction(t *testing.T) {
+	ui := new(cli.MockUi)
+	c := &BucketCommand{
+		UI: ui,
+	}
+
+	args := []string{
+		"--bucket", "foo",
+		"--region", "region1",
+		"--action", "reset",
+	}
+
+	if code := c.Run(args); code != 1 {
+		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	}
+}
